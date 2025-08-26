8500742d894493655de4d70e8fb86f376df03f76
Han Lee <bearchanlee@gmail.com>
2025-08-25 17:33:51 -0700

    added live stripepriceid

--- actions/stripe.ts
+++ actions/stripe.ts
@@ -4,6 +4,10 @@ import { stripe } from "../providers/stripe/stripe";
 import { createServerAction } from "zsa";
 
 export const stripeAction = createServerAction().handler(async () => {
+  const priceId =
+    process.env.NODE_ENV === "production"
+      ? "price_1S0Amr50xduodntUOPvUJ2Wc"
+      : "price_1S06Y08ivRxcR8RmJubNRaVF";
   const origin =
     process.env.NODE_ENV === "production"
       ? "https://sgmchurch.com"
@@ -13,7 +17,8 @@ export const stripeAction = createServerAction().handler(async () => {
     ui_mode: "embedded",
     line_items: [
       {
-        price: "price_1S06Y08ivRxcR8RmJubNRaVF",
+        // price: "price_1S06Y08ivRxcR8RmJubNRaVF",
+        price: priceId,
         quantity: 1,
       },
     ],
